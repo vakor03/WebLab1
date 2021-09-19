c8fef0b73bbd623c9ac3c23849e54819687da3a3
Vakor <myksv200@gmail.com>
2021-09-19 13:22:35 +0300

    eslint + package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,22 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
     "extends": [
-        "standard"
+        "eslint:recommended",
+        "plugin:react/recommended"
     ],
     "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
         "ecmaVersion": 12,
         "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
     "rules": {
     }
-}
+};
